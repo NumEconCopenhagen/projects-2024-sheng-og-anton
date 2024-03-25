caaf60c7947c945b873f2b54508dd2f7eac47056
Anton Meier Ebsen Jørgensen <133022210+AntonEbsen@users.noreply.github.com>
2024-03-25 14:06:38 +0100

    Updated bug

--- inauguralproject/Question7.py
+++ inauguralproject/Question7.py
@@ -10,10 +10,25 @@ class TotalEndowments:
 
     # b. We plot the figure
     def plot_endowments(self, wA1, wA2):
-        plt.figure(figsize=(8, 6))
-        plt.scatter(wA1, wA2, c='green')
-        plt.title('The set of endowments with 50 elements')
-        plt.xlabel('ωA1')
-        plt.ylabel('ωA2')
-        plt.grid(True)
-        plt.show()
+        result = self.pareto_improvements()
+
+        # Plot the Edgeworth box with the endowments
+        fig, ax = plt.subplots(figsize=(8, 8))
+        ax.set_xlabel("$x_1^A$") # setting x-axis label
+        ax.set_ylabel("$x_2^A$") # setting y-axis label
+        # Setting the limits
+        ax.set_xlim(0, 1)
+        ax.set_ylim(0, 1)
+
+        # Plotting endowment points
+        ax.scatter(self.w_A1, self.w_A2, marker='s', color='black', label='Endowment A')
+        ax.scatter(self.w_B1, self.w_B2, marker='s', color='red', label='Endowment B')
+
+        # Plotting Pareto improvements
+        for allocation in result:
+            ax.scatter(allocation[0], allocation[1], color='green') 
+
+        ax.legend() # adding legend
+        plt.show() # display the plot
+
+    
