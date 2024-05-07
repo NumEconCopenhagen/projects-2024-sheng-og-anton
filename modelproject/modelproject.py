7a51d2d37e04adf284aee39b3e83394cf43157de
AntonEbsen <anton.ebsen@gmail.com>
2024-05-07 12:51:20 +0200

    Adding more code

--- modelproject/modelproject.py
+++ modelproject/modelproject.py
@@ -34,83 +34,83 @@ class RealBusinessCycleModel(object):
         # Fifth element is technology
         self.technology = params[4]
         
-    def logged_(self, next_period_log, this_period_log):
+    def logged_variables(self, next_period_log_variables, this_period_log_variables):
         (next_period_log_output, next_period_log_consumption, next_period_log_investment,
-         log_lead_labor, log_lead_leisure, log_lead_capital) = next_period_log
+         next_period_log_labour, next_period_log_leisure, next_period_log_capital) = next_period_log_variables
         
-        (log_output, log_consumption, log_investment, log_labor,
-         log_leisure, log_capital) = this_period_log
+        (this_period_log_output, this_period_log_consumption, this_period_log_investment, this_period_log_labour,
+         this_period_log_leisure, this_period_log_capital) = this_period_log_variables
         
         return np.r_[
-            self.log_static_foc(
+            self.log_first_order_condition(
                 next_period_log_consumption, next_period_log_labour,
                 next_period_log_capital
             ),
             self.log_euler_equation(
-                log_lead_consumption, next_period_labor,
-                log_lead_capital, next_period_log_consumption
+                next_period_log_consumption, next_period_log_labour,
+                next_period_log_capital, next_period_log_consumption
             ),
-            self.log_production(
-                log_lead_output, log_lead_labor, log_lead_capital
+            self.log_production_function(
+                next_period_log_output, next_period_log_labour, next_period_log_capital
             ),
             self.log_aggregate_resource_constraint(
-                log_lead_output, log_lead_consumption,
-                log_lead_investment
+                next_period_log_output, next_period_log_consumption,
+                next_period_log_investment
             ),
             self.log_capital_accumulation(
-                log_lead_capital, log_investment, log_capital
+                next_period_log_capital, next_period_log_investment, next_period_log_capital
             ),
             self.log_labor_leisure_constraint(
-                log_lead_labor, log_lead_leisure
+                next_period_log_labour, next_period_log_leisure
             ),
         ]
     
-    def log_static_foc(self, log_lead_consumption, log_lead_labor,
-                       log_lead_capital):
+    def log_first_order_condition(self, next_period_log_consumption, next_period_log_labour,
+                       next_period_log_capital):
         return (
-            np.log(self.disutility_labor) +
-            log_lead_consumption -
+            np.log(self.disutility_from_labour) +
+            next_period_log_consumption -
             np.log(1 - self.capital_share) -
-            log_lead_technology_shock -
+            self.technology -
             self.capital_share * (log_lead_capital - log_lead_labor)
         )
         
-    def log_euler_equation(self, log_lead_consumption, log_lead_labor,
-                           log_lead_capital, log_consumption):
+    def log_euler_equation(self, next_period_log_consumption, next_period_log_labour,
+                            next_period_log_capital, next_period_log_consumption):
         return (
-            -log_consumption -
+            -this_period_log_consumption -
             np.log(self.discount_rate) +
-            log_lead_consumption -
+            next_period_log_consumption -
             np.log(
                 (self.capital_share * 
-                 np.exp((1 - self.capital_share) * log_lead_labor) /
-                 np.exp((1 - self.capital_share) * log_lead_capital)) +
+                 np.exp((1 - self.capital_share) * next_period_log_labour) /
+                 np.exp((1 - self.capital_share) * next_period_log_capital)) +
                 (1 - self.depreciation_rate)
             )
         )
         
-    def log_production(self, log_lead_output, log_lead_labor, log_lead_capital):
+    def log_production_function(self, next_period_log_output, next_period_log_labour, next_period_log_capital):
         return (
-            log_lead_output - self.capital_share * log_lead_capital -
-            (1 - self.capital_share) * log_lead_labor
+            next_period_log_output - self.capital_share * next_period_log_capital -
+            (1 - self.capital_share) * next_period_log_labour
         )
         
-    def log_aggregate_resource_constraint(self, log_lead_output, log_lead_consumption,
-                                          log_lead_investment):
+    def log_aggregate_resource_constraint(self, next_period_log_output, next_period_log_consumption,
+                                          next_period_log_investment):
         return (
-            log_lead_output -
-            np.log(np.exp(log_lead_consumption) + np.exp(log_lead_investment))
+            next_period_log_output -
+            np.log(np.exp(next_period_log_consumption) + np.exp(next_period_log_investment))
         )
     
-    def log_capital_accumulation(self, log_lead_capital, log_investment, log_capital):
+    def log_capital_accumulation(self, next_period_log_capital, this_period_log_investment, this_period_log_capital):
         return (
             log_lead_capital -
-            np.log(np.exp(log_investment) + (1 - self.depreciation_rate) * np.exp(log_capital))
+            np.log(np.exp(this_period_log_investment) + (1 - self.depreciation_rate) * np.exp(this_period_log_capital))
         )
     
-    def log_labor_leisure_constraint(self, log_lead_labor, log_lead_leisure):
+    def log_labour_leisure_constraint(self, next_period_log_labour, log_lead_leisure):
         return (
-            -np.log(np.exp(log_lead_labor) + np.exp(log_lead_leisure))
+            -np.log(np.exp(next_period_log_labour) + np.exp(next_period_log_leisure))
         )
 class NumericalSolution(RealBusinessCycleModel):
     def steady_state_numeric(self):
